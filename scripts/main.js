11c94705827e0a0c09d39c4bbb4d8f938e3d7d72
Gerry-Straatsma <67235321+Gerry-Straatsma@users.noreply.github.com>
2020-06-29 14:26:23 -0400

    Added content, edited the scrollbar, finished the nav bar

--- scripts/main.js
+++ scripts/main.js
@@ -7,4 +7,58 @@ function start() {
   titleCont.style.padding = divideHeight + "px 0px";
 }
 
+
+function scroll() {
+  var scroll = window.pageYOffset;
+  stickyNav(scroll);
+  getIDLocations(scroll);
+}
+
+function getIDLocations(scroll) {
+  skillsOffset = document.getElementById('skills').offsetTop;
+  projectsOffset = document.getElementById('projects').offsetTop;
+  resumeOffset = document.getElementById('resume').offsetTop;
+  aboutOffset = document.getElementById('about').offsetTop;
+
+  home = document.getElementById('homeOption');
+  skills = document.getElementById('skillsOption');
+  projects = document.getElementById('projectsOption');
+  resume = document.getElementById('resumeOption');
+  about = document.getElementById('aboutOption');
+
+  home.classList.remove("active");
+  skills.classList.remove("active");
+  projects.classList.remove("active");
+  resume.classList.remove("active");
+  about.classList.remove("active");
+
+  if (skillsOffset <= scroll) {
+    skills.classList.add("active");
+    if (projectsOffset <= scroll) {
+      projects.classList.add("active");
+      if (resumeOffset <= scroll) {
+        resume.classList.add("active");
+        if (aboutOffset <= scroll) {
+          about.classList.add("active");
+        }
+      }
+    }
+  } else {
+    home.classList.add("active");
+  }
+}
+
+function stickyNav(scroll) {
+  nav = document.getElementById('nav');
+  navCont = document.getElementById('navCont');
+  header = document.getElementById('header');
+  header = header.offsetHeight;
+  if (scroll > header - 200) {
+    navCont.classList.add("sticky");
+  } else {
+    navCont.classList.remove("sticky");
+  }
+}
+
 window.onload = start;
+window.onscroll = function() {scroll()};
