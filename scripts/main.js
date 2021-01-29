6fcd186c9939b84e000e6a5801828d87b3d92d3b
Gerry-Straatsma <67235321+Gerry-Straatsma@users.noreply.github.com>
2021-01-28 23:31:46 -0500

    First update of the new site

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+function scroll() {
+  var scroll = window.pageYOffset;
+  getIDLocations(scroll);
+}
+
+function getIDLocations(scroll) {
+  skillsOffset = document.getElementById('skills').offsetTop;
+  projectsOffset = document.getElementById('projects').offsetTop;
+  resumeOffset = document.getElementById('resume').offsetTop;
+  aboutOffset = document.getElementById('about').offsetTop;
+
+  home = document.getElementById('homeOption');
+  skills = document.getElementById('skillsOption');
+  projects = document.getElementById('projectsOption');
+  resume = document.getElementById('resumeOption');
+  about = document.getElementById('aboutOption');
+
+  home.classList.remove("active");
+  skills.classList.remove("active");
+  projects.classList.remove("active");
+  resume.classList.remove("active");
+  about.classList.remove("active");
+
+  if (aboutOffset <= scroll) {
+    about.classList.add("active");
+    if (resumeOffset - 1 <= scroll) {
+      about.classList.remove("active");
+      resume.classList.add("active");
+      if (skillsOffset <= scroll) {
+        resume.classList.remove("active");
+        skills.classList.add("active");
+        if (projectsOffset <= scroll) {
+          skills.classList.remove("active");
+          projects.classList.add("active");
+        }
+      }
+    }
+  } else {
+    home.classList.add("active");
+  }
+}
+
+window.onscroll = function() {scroll()};
