3bd3f9594ed49ed2dd207455e3306ce9f76e1750
Gerry-Straatsma <67235321+Gerry-Straatsma@users.noreply.github.com>
2023-08-10 18:55:16 -0400

    Cleaned up files

--- Archive/scripts/email.js
+++
@@ -1,9 +0,0 @@
-function sendMail() {
-  name = document.forms["mailForm"]["name"].value;
-  email = document.forms["mailForm"]["email"].value;
-  body = document.forms["mailForm"]["body"].value;
-  console.log(name);
-  console.log(email);
-  console.log(body);
-  return false;
-}
